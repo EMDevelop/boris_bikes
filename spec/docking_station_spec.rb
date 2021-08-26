00a0bb20dbe8d8738af2a89260de3c45eac44856
Amanda Vuong <mandyvuong@gmail.com>
2021-08-26 15:31:01 +0100

    step 11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,7 @@ require 'docking_station'
 
 describe DockingStation do
 
-
+# act attr_reader
   context '.release_bike' do
     it 'checks that release bike method exists' do
       expect(subject).to respond_to (:release_bike)
@@ -18,8 +18,20 @@ describe DockingStation do
       expect(bike.working?).to eq true
     end
 
+    it { is_expected.to respond_to(:dock).with(1).argument} 
+    
+    it { is_expected.to respond_to(:bike)}
 
+    it 'docks something' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+
+    it 'returns docked bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end
+    
   end
-
-
 end
